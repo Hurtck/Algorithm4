7dd97ecc10c89f9d4a5312482d16624057ae04a9
hurtck <luogs1102@foxmail.com>
2018-10-08 20:17:42 +0800

    sort-list

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,24 +1,32 @@
 public class MergeSort {
-    public void sort(int[] a, int lo,int hi){
-        if (lo<=hi) return;
+    public static void main(String[] args) {
+        int a[] = {8,48,2,3,4,91,8,1,2,8};
+        sort(a,0,a.length-1);
+        for(int i:a){
+            System.out.println(i);
+        }
+    }
+    public static void sort(int[] a, int lo,int hi){
+        if (hi<=lo) return;
         int mid = (lo+hi)/2;
         sort(a,lo,mid);
         sort(a,mid+1,hi);
         merge(a,lo,mid,hi);
     }
 
-    private void merge(int[] a, int lo, int mid,int hi) {
-        int[] au = new int[a.length];
+    private static void merge(int[] a, int lo, int mid,int hi) {
+        int[] au = new int[hi-lo+1];
+
         int l = lo;
-        int h = mid;
-        for(int i = 0;i<a.length;i++){
-            au[i] =au[i];
+        int h = mid+1;
+        for(int i = lo;i<=hi;i++){
+            au[i-lo] =a[i];
         }
-        for(int i = 0;i<a.length;i++){
-            if(l==mid){a[i] = au[h];h++;}
-            else if(h == hi+1){a[i] = au[l];l++;}
-            else if(au[l]>au[h]) {a[i]=au[h];h++;}
-            else if(au[h]>au[l]) {a[i]=au[l];l++;}
+        for(int i = lo;i<=hi;i++){
+            if(l > mid){a[i] = au[h-lo];h++;}
+            else if(h > hi){a[i] = au[l-lo];l++;}
+            else if(au[l-lo]>au[h-lo]) {a[i]=au[h-lo];h++;}
+            else {a[i]=au[l-lo];l++;}
         }
     }
 }
--- src/leetcode/Ace.java
+++ src/leetcode/Ace.java
@@ -4,12 +4,132 @@ import leetcode.TreeNode;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Ace {
     public static void main(String[] args) {
+        //(0,9),(138,429),(115,359),(115,359),(-30,-102),(230,709),(-150,-686),(-135,-613),(-60,-248),(-161,-481),(207,639),(23,79),(-230,-691),(-115,-341),(92,289),(60,336),(-105,-467),(135,701),(-90,-394),(-184,-551),(150,774)
+        Point[] points = new Point[]{new Point(0,9),new Point(138,429),new Point(115,359),
+                new Point(115,359),new Point(-30,-102),new Point(230,709),
+                new Point(-150,-686),new Point(-135,-613),new Point(-60,-248),
+                new Point(-161,-481),new Point(207,639),new Point(23,79),
+                new Point(-230,-691),new Point(-115,-341),new Point(92,28),new Point(60,336),
+                new Point(-105,-467),new Point(135,701),new Point(-90,-394),new Point(-184,-551),new Point(150,774)
+        };
+        System.out.println(maxPoints(points));
+    }
+
+    public ListNode sortList(ListNode head) {
+        if(head == null||head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode midNext = mid.next;
+        mid.next = null;
+        ListNode preList = sortList(head);
+        ListNode lastList = sortList(midNext);
+        return mergeSort(preList,lastList);
+    }
+    private ListNode getMid(ListNode head) {
+        if(head == null||head.next == null)
+            return head;
+        ListNode quickNode = head;
+        ListNode slowNode = head;
+        while (quickNode !=null&&quickNode.next!=null){
+            quickNode = quickNode.next.next;
+            slowNode = slowNode.next;
+        }
+        return slowNode;
+    }
+    private ListNode mergeSort(ListNode n1, ListNode n2) {
+        ListNode preList = n1,lastList = n2;
+        ListNode head = new ListNode(0);
+        ListNode curList = head;
+        while (preList !=null&&lastList!=null){
+            if(preList.val>lastList.val){
+                curList.next = lastList;
+                lastList = lastList.next;
+            }else {
+                curList.next = preList;
+                preList = preList.next;
+            }
+            curList = curList.next;
+        }
+        if(preList == null) curList.next = lastList;
+        else curList.next = preList;
+        return head.next;
+    }
+
+    //max-points-on-a-line
+    public static int maxPoints(Point[] points) {
+        if(points.length<=2) return points.length;
+
+        int maxNumbers = 2;
+        for(int i=0;i<points.length;i++){
+            for(int j=i+1;j<points.length;j++){
+                int count = 2;
+                for(int k = j+1;k<points.length;k++){
+                    //重复点直接++
+                    if ((points[k].x == points[i].x && points[k].y == points[i].y) ||
+                            (points[k].x == points[j].x && points[k].y == points[j].y)) {
+                        count++;
+                        if (count > maxNumbers)
+                            maxNumbers = count;
+                        continue;
+                    }
+
+                    //垂直点判断
+                    if (points[k].x == points[j].x) {
+                        if (points[j].x == points[i].x) {
+                            count++;
+                            if (count > maxNumbers)
+                                maxNumbers = count;
+                            continue;
+                        } else
+                            continue;
+                    } else if (points[j].x == points[i].x) {
+                        continue;
+                    }
+
+                    if((points[k].y-points[j].y)/(float)(points[k].x-points[j].x) ==
+                            (points[j].y-points[i].y)/(float)(points[j].x-points[i].x)) count++;
+                    if(count>maxNumbers) maxNumbers = count;
+                }
+            }
+        }
+        return maxNumbers;
+
     }
 
 
+    //evaluate-reverse-polish-notation
+    public int evalRPN(String[] tokens) {
+        Stack<String> stack = new Stack<>();
+        for(String symbol:tokens){
+            if("+".equals(symbol)){
+                int a = Integer.valueOf(stack.pop());
+                int b =Integer.valueOf(stack.pop());
+                stack.push((b+a)+"");
+            }
+            else if("-".equals(symbol)){
+                int a = Integer.valueOf(stack.pop());
+                int b =Integer.valueOf(stack.pop());
+                stack.push((b-a)+"");
+            }
+            else if("*".equals(symbol)){
+                int a = Integer.valueOf(stack.pop());
+                int b =Integer.valueOf(stack.pop());
+                stack.push((b*a)+"");
+            }
+            else if("/".equals(symbol)){
+                int a = Integer.valueOf(stack.pop());
+                int b =Integer.valueOf(stack.pop());
+                stack.push((b/a)+"");
+            }else stack.push(symbol);
+        }
+        return Integer.valueOf(stack.pop());
+    }
+
+    //minimum-depth-of-binary-tree
     public int run(TreeNode root) {
         Queue<TreeNode> list = new LinkedList<>();
         if (root  == null) return 0;
@@ -32,7 +152,7 @@ public class Ace {
         }
         return depth;
     }
-
+    //二维数组寻找最小值
     public static boolean Find(int target, int [][] array) {
         int rowMaxIndex = array.length-1;
         int listMaxtIndex = array[0].length-1;
@@ -63,5 +183,20 @@ public class Ace {
 //        }
 //        return false;
     }
+
+    static class Point {
+        int x;int y;
+        Point() { x = 0; y = 0; }
+        Point(int a, int b) { x = a; y = b; }
+    }
+
+     class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
 
 }
